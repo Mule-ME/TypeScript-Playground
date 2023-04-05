f3fe91f459da8596afd7ddcf75e1a81f34ffe7c8
Mule-ME <mulualemesh15@gmail.com>
2023-04-05 17:34:40 +0300

    Learning about tuples

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
- let age: number = 20
-  if(age<50)
-  age +=10
+let age: number = 20
+if (age < 50)
+  age += 10
 console.log(age)
+
+
+//Types
+//1,any 
+// variable declared without any type and without any initialization //eg=>
+let result  //which is === let result: any
+
+
+//2, Arrays
+let numbers: number[] = [1, 2, 3]
+
+//3,Tuples
+//
+
+const myCollection:[string, number] = ["Yosef", 1] 
+
+
+
