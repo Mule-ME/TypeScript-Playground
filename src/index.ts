b73ebec4fffa67a2173daee0f2020e22c2894e9e
Mule-ME <mulualemesh15@gmail.com>
2023-04-06 14:47:46 +0300

    Learning TypeScript:- functions

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,22 @@ const myCollection: [string, number] = ["Mulualem", 1]
 //PascalCase
 enum Size { Small = 1, Medium, Large } //define it as a constant to get  the most out of it while compiling it will help as to hide abstraction of how enum define in js
 let mySize: Size = Size.Medium
-
 console.log(mySize) // o/p = 2
 
+//Functions
+function calculateTac(
+  income: number,
+  taxYear = 2019,
+  unUsedParam =1  // to tackle this error turn on //"noUnusedParameters": true, 1
+): number // to tackle this error turn on //"noImplicitReturns": true, 2.
+
+{
+
+  let x; // to tackle this error turn on //"noUnusedLocals": true, 3.
+  if (taxYear < 2022)
+    return income * 1.2
+  // return income * 1.3
+}
+console.log(calculateTac(10_000)) //or
+console.log(calculateTac(10_000, 2023))
 
