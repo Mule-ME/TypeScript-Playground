d01e62caad54e71e5347f92c21271f5860b6ecd6
Mule-ME <mulualemesh15@gmail.com>
2023-04-06 14:30:22 +0300

    Learning TypeScript:- enum

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,9 @@ let age = 20;
 if (age < 50)
     age += 10;
 console.log(age);
+let result;
+let numbers = [1, 2, 3];
+const myCollection = ["Mulualem", 1];
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,20 @@ let numbers: number[] = [1, 2, 3]
 
 //3,Tuples
 //if we have a fixed number of element must have 2 input
-const myCollection:[string, number] = ["Yosef", 1] 
+const myCollection: [string, number] = ["Mulualem", 1]
 
+//4, Enum 
+//which represent a list of related constants e.g =>
+
+//Don't have to use the bellow
+// const small =1 
+// const medium = 2
+// const large = 3 simply we can define these constants using enum
+
+//PascalCase
+enum Size { Small = 1, Medium, Large } //define it as a constant to get  the most out of it while compiling it will help as to hide abstraction of how enum define in js
+let mySize: Size = Size.Medium
+
+console.log(mySize) // o/p = 2
 
 
