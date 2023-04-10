dbae100806e5a2a6aae1acff6b5df6b081cb4daa
Mule-ME <mulualemesh15@gmail.com>
2023-04-10 12:33:01 +0300

    Learning TypeScript:- OOP ->Indexing signature and  Static members

--- src/objetOrientedProgramming.ts
+++ src/objetOrientedProgramming.ts
@@ -42,6 +42,11 @@ export class Account {
     return this._balance;
   }
 
+  set balance(value: number) {
+    if (value < 0) throw new Error("Amount must be greater than 0");
+    this._balance = value;
+  }
+
   //private method also work for methods
   private calculateInterest(balance: number, rate: number): number {
     return (balance * rate) / 100;
@@ -63,17 +68,56 @@ class Person {
     public age: number,
     private _password: string
   ) {}
-//<------- Getters and Setters --------->
-//getter a method inside of a class to get a value of a properties
- get hashPassword () {
-    const hashKey = "734rygf7vs5tgr3f7d6w534trgfw7y65t3grf"
-    return this._password.concat(hashKey);
- }
 
+  //<------- Getters and Setters --------->
+  //getter a method inside of a class to get a value of a properties
+  get password() {
+    return this._password;
+  }
+
+  set password(password: string) {
+    const hashKey = "734rygf7vs5tgr3f7d6w534trgfw7y65t3grf";
+    password.concat(hashKey);
+  }
 }
 
 const person = new Person(1, "Mulualem", 20, "123456");
+console.log(person.name);
 
-console.log(person.name) 
- 
+//<------- Indexing Signatures --------->
+//it help us to add or change properties of objects dynamically
+class SeatAssignment {
+  //INdex signature property
+  [seatNumber: string]: string;
+}
 
+let seats = new SeatAssignment();
+seats["A1"] = "Mulualem";
+seats.A2 = "Eshetu";
+
+//<------- Static Members --------->
+//It is a property that belongs to a class not an object,
+//once we create a property with static it became part of a class it have only a single instance of it on memory
+
+class Ride {
+  private static _activeRides: number = 0;
+
+  start() {
+    Ride._activeRides++;
+  }
+  end() {
+    Ride._activeRides--;
+  }
+
+  static get activeRides() {
+    return Ride._activeRides;
+  }
+}
+
+const ride1 = new Ride();
+ride1.start();
+
+const ride2 = new Ride()
+ride2.start();
+
+console.log(Ride.activeRides)
