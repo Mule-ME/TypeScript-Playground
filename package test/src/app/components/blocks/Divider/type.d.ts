c897c8b10cc5dddba0ddaaf8e03be2d76df8abf6
Mule-ME <mulualemesh15@gmail.com>
2023-12-16 00:59:37 +0300

    fix: cleaning up unwanted files and fixing some content

--- package test/src/app/components/blocks/Divider/type.d.ts
+++
@@ -1,9 +0,0 @@
-
-import { LayoutProps} from 'styled-system';
-
-export interface DividerProps extends LayoutProps {
-  orientation: 'horizontal' | 'vertical';
-  color?: string;
-  thickness?: number;
-  size?: number
-}
